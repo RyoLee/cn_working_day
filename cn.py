a6decee5a4cb55c1b7be321959754a0685574fe4
Lee <516127941@qq.com>
2022-04-29 17:08:07 +0800

    init

---
+++ cn.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python
+# -*- coding: UTF-8 -*-
+
+from workalendar.asia import China
+from datetime import timedelta, date
+import json
+if __name__ == "__main__":
+    cal = China()
+    cur = date.today() - timedelta(days=15)
+    res = {}
+    for i in range(30):
+        cur += timedelta(days=1)
+        res[str(cur)] = cal.is_working_day(cur)
+    with open('cn.json', 'w') as json_file:
+        json.dump(res, json_file, indent=4, sort_keys=True)
